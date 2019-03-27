00a58400c36bd04ab7c69e7067f847a6bf1d973f
paul wisehart <paul@oldcode.org>
2019-03-27 01:34:03 -0400

    no date web.RenderPage works

--- web/web.go
+++ web/web.go
@@ -5,20 +5,37 @@ import (
 	"net"
 	"net/http"
 	"net/http/fcgi"
+	"strings"
 	"text/template"
 )
 
 var tmpls map[string]*template.Template
 
+func GetTmpls() map[string]*template.Template {
+	return tmpls
+}
+
 func init() {
 	tmpls = make(map[string]*template.Template)
+}
+
+func RenderPage(page string, w http.ResponseWriter) {
+	headers := w.Header()
+	headers.Add("Content-Type", "text/html")
+	_, ok := tmpls[page]
+	if !ok {
+		ss := []string{"tmpl/", page, ".html"}
+		tmpls[page] = template.Must(template.ParseFiles("tmpl/base.html",
+			strings.Join(ss, "")))
+	}
+	tmpls[page].Execute(w, nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	headers := w.Header()
 	headers.Add("Content-Type", "text/html")
 	_, ok := tmpls["index"]
-	if (!ok) {
+	if !ok {
 		tmpls["index"] = template.Must(template.ParseFiles("tmpl/base.html", "tmpl/index.html"))
 	}
 	tmpls["index"].Execute(w, nil)
@@ -32,12 +49,19 @@ func h1(w http.ResponseWriter, r *http.Request) {
 }
 
 func other(w http.ResponseWriter, r *http.Request) {
-	tmpl := make(map[string]*template.Template)
-	tmpl["other.html"] = template.Must(template.ParseFiles("tmpl/base.html", "tmpl/other.html"))
+	_, ok := tmpls["other"]
+	if !ok {
+		tmpls["other"] = template.Must(
+			template.ParseFiles("tmpl/base.html", "tmpl/other.html"))
+	}
 	headers := w.Header()
 	headers.Add("Content-Type", "text/html")
 	//tmpl["other.html"].ExecuteTemplate(w, "name-of-define-block", nil)
-	tmpl["other.html"].Execute(w, nil)
+	tmpls["other"].Execute(w, nil)
+}
+
+func page3(w http.ResponseWriter, r *http.Request) {
+	RenderPage("page3", w)
 }
 
 func Serve() {
@@ -48,5 +72,6 @@ func Serve() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/h1", h1)
 	http.HandleFunc("/other", other)
+	http.HandleFunc("/page3", page3)
 	fcgi.Serve(listener, nil)
 }
