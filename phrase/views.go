7ec407e9d52d963596361744791711959521776e
paul wisehart <paul@oldcode.org>
2019-10-17 06:59:45 -0400

    /phrase initial codemirror editor

--- phrase/views.go
+++ phrase/views.go
@@ -3,6 +3,7 @@ package phrase
 import (
 	"net/http"
 
+	"github.com/justinas/nosurf"
 	"oldcode.org/home/wise/repo/go/oldcode.org/gow/breadcrumbs"
 	"oldcode.org/home/wise/repo/go/oldcode.org/gow/web"
 )
@@ -14,6 +15,7 @@ func AddRoutes(mux *http.ServeMux) {
 func Index(w http.ResponseWriter, r *http.Request) {
 	data, _ := web.TmplData(r)
 	data["breadcrumbs"] = breadcrumbs.New().Append("Home", "/").AppendActive("Phrase")
+	data["token"] = nosurf.Token(r)
 	tmpls := []string{
 		"base.html",
 		"breadcrumbs.tmpl",
