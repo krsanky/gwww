5c3c9868c1dcc4be5c005b7467b1c7f85fc696f3
paul wisehart <paul@oldcode.org>
2019-10-21 04:49:09 -0400

    reorg gow_test

--- cmd/gow_test/main.go
+++ cmd/gow_test/main.go
@@ -44,6 +44,20 @@ func dbstuff() {
 	}
 }
 
+func func_nil_string_test(str string) {
+	fmt.Printf("--- func_nil_string_test(str string) ---\n")
+	fmt.Printf("str:%s\n", str)
+}
+
+func nil_string_test() {
+	fmt.Printf("call func_nil_string_test(str string) with string:\n")
+	func_nil_string_test("this-is-a-string asd asd") 
+
+	fmt.Printf("call func_nil_string_test(str string) with nil:\n")
+	//func_nil_string_test(nil)
+	fmt.Printf("won't compile :)\n")
+}
+
 func main() {
 	settings.Init("settings.toml")
 	db.InitDB()
@@ -54,6 +68,10 @@ func main() {
 		case "db":
 			//db_artists()
 			dbstuff()
+		case "nil-string":
+			nil_string_test()
+		case "help":
+			fmt.Printf("\ngow_test <tmpl|db|nil-string|help>\n\n")
 		default:
 			test_phrase()
 		}
