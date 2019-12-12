9d69e368e02eded0cd1daa3c2050440ce7700f72
paul wisehart <paul@oldcode.org>
2019-12-12 01:35:44 -0500

    working phrase/lorem template tags

--- phrase/tmpl/tmpl.go
+++ phrase/tmpl/tmpl.go
@@ -1,23 +1,41 @@
 package tmpl
 
 import (
-	"fmt"
 	"html/template"
+	"strings"
 
-	"oldcode.org/repo/go/gow/lg"
 	krs_lorem "github.com/krsanky/lorem"
 )
 
 func AddFuncs(fm template.FuncMap) {
-	fm["lorem"] = lorem
-	fm["lorems"] = lorems
+	fm["lorem_w"] = lorem_w
+	fm["lorem_ws"] = lorem_ws
+	fm["lorem_s"] = lorem_s
+	fm["lorem_p"] = lorem_p
+	fm["lorem_ps"] = lorem_ps
 }
 
-func lorem(a bool) template.HTML {
-	lg.Log.Printf("lorem active:%v", a)
-	return template.HTML(fmt.Sprintf("<h1>x active %v</h1>", a))
+func lorem_w() template.HTML {
+	w := krs_lorem.Words(1, false)
+	return template.HTML(w[0])
 }
 
-func lorems() template.HTML {
+func lorem_ws(n int, r bool) template.HTML {
+	ws := krs_lorem.Words(int(n), !r)
+	out := strings.Join(ws, " ")
+	return template.HTML(out)
+}
+
+func lorem_s() template.HTML {
 	return template.HTML(krs_lorem.Sentence())
+}
+
+func lorem_p() template.HTML {
+	return template.HTML(krs_lorem.Paragraph())
+}
+
+func lorem_ps(count int, common bool) template.HTML {
+	ps := krs_lorem.Paragraphs(count, !common)
+	out := strings.Join(ps, "\n\n")
+	return template.HTML(out)
 }
