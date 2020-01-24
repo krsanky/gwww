38f5dc50268078a065a9e06862ee067a203b532d
paul wisehart <paul@oldcode.org>
2020-01-24 05:53:35 -0500

    add BSD copyright to source code

--- secure/middleware.go
+++ secure/middleware.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2020 Paul Wisehart paul@oldcode.org
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 package secure
 
 import (
@@ -6,6 +20,7 @@ import (
 	"github.com/krsanky/gwww/account"
 	"github.com/krsanky/gwww/lg"
 )
+
 /*
 three options to secure a page:
 1. path based.  This is easy but less secure, because if
@@ -45,4 +60,3 @@ func SuperOnlyMiddleware(h http.Handler) http.Handler {
 func SuperOnly(handler func(http.ResponseWriter, *http.Request)) http.Handler {
 	return SuperOnlyMiddleware(http.HandlerFunc(handler))
 }
-
